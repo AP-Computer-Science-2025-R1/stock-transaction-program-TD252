37901dc7d4696f551e21c29844d8de6555c31049
TD252 <Tarandeepd@nycstudents.net>
2025-09-30 09:08:59 -0400

    Update Main.py

--- Main.py
+++ Main.py
@@ -9,13 +9,14 @@ purchase_commission = purchase_total * 0.02
 sale_total = shares * sale_price
 sale_commission = sale_total * commission_rate
 profit_or_loss = (sale_total - sale_commission) - (purchase_total + purchase_commission)
-print(f"Amount paid for the stock: ${purchase_total:.2f}")
-print(f"Commission paid on purchase: ${purchase_commission:.2f}")
-print(f"Amount stock sold for: ${sale_total:.2f}")
-print(f"Commission paid on sale: ${sale_commission:.2f}")
-print(f"Profit or Loss: ${profit_or_loss:.2f}")
+print(f"Amount paid for the stock: ${purchase_total:}")
+print(f"Commission paid on purchase: ${purchase_commission:}")
+print(f"Amount stock sold for: ${sale_total:}")
+print(f"Commission paid on sale: ${sale_commission:}")
+print(f"Profit or Loss: ${profit_or_loss:}")
 
 if profit_or_loss > 0:
     print("Joe made a profit.")
 else:
-    print("Joe lost money.")
+    print("Joe lost money.")    
+
